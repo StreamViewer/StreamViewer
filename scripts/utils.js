0581e2376d8710186c101b05898f0b42314d7ff8
idlys <rorylit@gmail.com>
2015-08-27 12:28:12 -0700

    No more 'ID: unknown', spacing tweaked

---
+++ scripts/utils.js
@@ -0,0 +1,18 @@
+function setCookie(name, value, expire) {
+    var d = new Date();
+    d.setTime(d.getTime() + expire);
+
+    document.cookie = name + '=' + value + ';' + 'expires=' + d.toUTCString();
+}
+
+function getCookie(name) {
+    var ca = document.cookie.split(';');
+
+    var i = ca.length;
+    while (i--) {
+        var c = ca[i].trim();
+        if (c.trim.indexOf(name) == 0) {
+            return c.substring(name.length + 1, c.length);
+        }
+    }
+}
